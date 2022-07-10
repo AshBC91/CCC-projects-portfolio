c53c8cb5ba724f1bc1478de9f5854bb8d574c50d
Ash <101850594+AshBC91@users.noreply.github.com>
2022-07-09 23:14:37 -0400

    adding ntro project

---
+++ CSCS1730_UNIX/projects/01ntr0/hello.c
@@ -0,0 +1,14 @@
+/*
+ * hello.c - the first best C program ever
+ *
+ * written by: Ashley Barb Caldelas
+ */
+#include <stdio.h>
+
+int main()					// every program needs a start, main() is ours
+{
+	fprintf(stdout, "¡Hola, mundo!\n"); 	// display a message to STDOUT
+	return(0);				// return a success status to OS
+}
+
+//end of line...
